040e8eddc0b611d2e470d7a6f431ef9f0c979050
Elliott Record <elliottrecord@gmail.com>
2020-11-21 18:50:37 +0000

    Removed currentNodeState from useBlackboards

--- frontend/hooks/useBlackboards.ts
+++ frontend/hooks/useBlackboards.ts
@@ -1,18 +1,15 @@
 import { ref } from '@nuxtjs/composition-api';
-import { NodeState } from '@yoody/behaviour-tree';
 import { IBlackboard } from '~/types';
 
 const DEFAULT_TARGET_COUNT = 10;
-const currentNodeState = ref<NodeState>();
 const currentCount = ref<number>(0);
 const targetCount = ref<number>(DEFAULT_TARGET_COUNT);
 
 export default function (): IBlackboard {
   const reset = () => {
     currentCount.value = 0;
-    currentNodeState.value = undefined;
     targetCount.value = DEFAULT_TARGET_COUNT;
   };
 
-  return { currentNodeState, currentCount, targetCount, reset };
+  return { currentCount, targetCount, reset };
 }
