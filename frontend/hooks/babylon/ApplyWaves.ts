32e9c8fa8bcf46666c29c3e4319b14c58c9d2259
Elliott Record <elliottrecord@gmail.com>
2020-12-07 00:45:42 +0000

    Updated the starting Position.Y for ApplyWaves hook

--- frontend/hooks/babylon/ApplyWaves.ts
+++ frontend/hooks/babylon/ApplyWaves.ts
@@ -1,29 +1,29 @@
-import useBabylon from '.';
 import { TransformNode } from 'babylonjs';
+import useBabylon from '.';
 
 export default function () {
   const { sceneRef } = useBabylon();
 
   const ApplySineWave = (node: TransformNode, incrementValue: number) => {
     let y = 0;
-    sceneRef.value.registerBeforeRender(() => {
-      node.position.y = Math.sin(y) * 5 + 10;
+    sceneRef.value!.registerBeforeRender(() => {
+      node.position.y = Math.sin(y) * 5 + 6;
       y += incrementValue;
     });
   };
 
   const ApplyCosineWave = (node: TransformNode, incrementValue: number) => {
     let y = 0;
-    sceneRef.value.registerBeforeRender(() => {
-      node.position.y = Math.cos(y) * 5 + 10;
+    sceneRef.value!.registerBeforeRender(() => {
+      node.position.y = Math.cos(y) * 5 + 6;
       y += incrementValue;
     });
   };
 
   const ApplyTanWave = (node: TransformNode, incrementValue: number) => {
     let y = 0;
-    sceneRef.value.registerBeforeRender(() => {
-      node.position.y = Math.tan(y) * 5 + 10;
+    sceneRef.value!.registerBeforeRender(() => {
+      node.position.y = Math.tan(y) * 5 + 6;
       y += incrementValue;
     });
   };
