852dcf651e3da5d78ff4d404f86945bff834f2bd
Elliott Record <elliottrecord@gmail.com>
2020-12-10 21:01:35 +0000

    Changed the color of a box within Babylon through the logic in a Behaviour Tree at /babylon-bt

---
+++ frontend/pages/babylon-bt.vue
@@ -0,0 +1,15 @@
+<template>
+  <canvas ref="canvasRef" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import scene from '../scenes/BehaviourTree';
+
+export default defineComponent({
+  setup() {
+    const { canvasRef } = scene();
+    return { canvasRef };
+  },
+});
+</script>
---
+++ frontend/scenes/BehaviourTree.ts
@@ -0,0 +1,55 @@
+import { computed, onMounted } from '@nuxtjs/composition-api';
+import { Color3, StandardMaterial } from 'babylonjs';
+import useBabylon from '../hooks/babylon';
+import useBT from '../trees/useChangeTrafficLightTree';
+
+export default () => {
+  const {
+    canvasRef,
+    sceneRef,
+    onHTMLReady,
+    useMeshBuilder,
+    useLightBuilder,
+    useCameraBuilder,
+    useApplyMaterial,
+    useApplyWaves,
+    useDTOBuilder,
+  } = useBabylon();
+
+  const { trafficLightState, startBehaviourTree, stopBehaviourTree } = useBT();
+
+  const { BuildArcRotateCamera } = useCameraBuilder();
+  const { BuildGround, BuildBox } = useMeshBuilder();
+  const { BuildHemisphericLight } = useLightBuilder();
+  const { ApplyGroundMaterial } = useApplyMaterial();
+  const { ApplySineWave, ApplyCosineWave, ApplyTanWave } = useApplyWaves();
+  const { BuildVector3 } = useDTOBuilder();
+
+  onMounted(() => {
+    onHTMLReady();
+
+    BuildArcRotateCamera(
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      10,
+      BuildVector3(5, 4, 0)
+    );
+
+    BuildHemisphericLight(BuildVector3(1, 1, 0));
+
+    const ground = BuildGround(10);
+    ApplyGroundMaterial(ground);
+
+    const box = BuildBox(BuildVector3(0, 0.5, 0), 1);
+    startBehaviourTree();
+
+    const color = new StandardMaterial('color', sceneRef.value!);
+    sceneRef.value?.onBeforeRenderObservable.add(() => {
+      const trafflicLightColorHex = trafficLightState.value;
+      color.diffuseColor = Color3.FromHexString(trafflicLightColorHex);
+      box.material = color;
+    });
+  });
+
+  return { canvasRef };
+};
