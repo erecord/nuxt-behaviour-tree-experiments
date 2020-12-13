0b7c8a0891956edd5f52fdb22e30d0a1566173c4
Elliott Record <elliottrecord@gmail.com>
2020-12-13 19:36:30 +0000

    Added page/flex.vue to text flexbox

---
+++ frontend/pages/flex.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col h-screen w-screen">
+    <div class="flex-grow bg-teal-700"></div>
+    <div class="flex-grow bg-blue-500"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({});
+</script>
