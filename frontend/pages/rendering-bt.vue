ceeac1ad12350aa195a32a41f0da84f9019466c5
Elliott Record <elliottrecord@gmail.com>
2020-12-15 21:27:09 +0000

    Added page for rendering-bt

---
+++ frontend/pages/rendering-bt.vue
@@ -0,0 +1,15 @@
+<template>
+  <BehaviourTree />
+</template>
+
+<script lang="ts">
+// Recursive
+// A reference to children, who also have a refernce to the parent
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
